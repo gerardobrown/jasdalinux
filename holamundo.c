df2f013abf34d610bc35c7b97e5a31044a4708af
gerardobrown <gerardobrown@gmail.com>
2026-07-29 15:32:26 -0600

    Tarea 01 - C y Python

---
+++ holamundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("-------------------------\n");
+    printf("ahora desde lenguaje c\n");
+    printf("Hola mundo\n");
+    printf("-------------------------\n");
+    return 0;
+}
+
